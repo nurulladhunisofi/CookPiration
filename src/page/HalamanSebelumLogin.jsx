1f7648a78cdda6b816f7f79870d5fed9897a4ed6
nurulladhunisofi <nurulladhunisofi@gmail.com>
2023-11-22 09:04:24 +0700

    membuat top resep mingguan

--- src/page/HalamanSebelumLogin.jsx
+++ src/page/HalamanSebelumLogin.jsx
@@ -5,9 +5,11 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Stack from 'react-bootstrap/Stack';
 //font awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight, faBookmark, faClock } from '@fortawesome/free-solid-svg-icons'
 const HalamanSebelumLogin = () => {
   return (
     <div>
@@ -25,7 +27,66 @@ const HalamanSebelumLogin = () => {
           </Col>
         </Row>
         <Row>
-          <h5>Top Resep Mingguan</h5>
+          <Col md={12}><h5>Top Resep Mingguan</h5></Col>
+          <Row className="m-auto">
+            <Col xs={6} md={3} className="mb-4">
+              <Card>
+                <Card.Img variant="top" src="/komponen-gambar/image 19.png" />
+                <Card.ImgOverlay className="w-25 text-center ms-auto">
+                  <Card.Text className="bg-dark text-light">
+                    <FontAwesomeIcon icon={faBookmark} />
+                  </Card.Text>
+                </Card.ImgOverlay>
+                <Card.Body>
+                  <Card.Title className="text-center"><h5>Gulai ayam</h5></Card.Title>
+                  <Card.Text>
+                    <FontAwesomeIcon icon={faClock} /> 25 Menit
+          
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={6} md={3} className="mb-4">
+              <Card>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={6} md={3} className="mb-4">
+              <Card>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={6} md={3} className="mb-4">
+              <Card>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
         </Row>
       </Container>
     </div>
